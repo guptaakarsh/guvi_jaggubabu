41b6d14991b6b3d986985b83f3ffcd7baef05350
guptaakarsh <35305373+guptaakarsh@users.noreply.github.com>
2018-01-21 01:00:31 +0530

    a

---
+++ oddno_interval.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int i,j,n,m;
+printf("enter 1st no");
+scanf("%d",&n);
+printf("enter 2nd no.");
+scanf("%d",&m);
+for(i=n;i<=m;i++)
+{
+	if(i%2!=0)
+	{
+		printf("%d",i);}
+		
+	else
+	 	{
+		continue;}
+	
+	
+}
+}
