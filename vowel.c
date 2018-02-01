2a7eeda05d1c6d7d4a25bc40b36edead23c13c9f
Jaggu <akarsh.gupta_cs15@gla.ac.in>
2018-02-01 23:54:36 +0530

    1

--- vowel.c
+++ vowel.c
@@ -5,8 +5,8 @@ void main()
     printf("enter the word:");
     scanf("%c",&ch);
     if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
-    printf("the word is vowel");
+    printf(" vowel");
     else
-printf("the word is cosnonent");
+printf(" cosnonent");
     
 }
